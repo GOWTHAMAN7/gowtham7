d9f497b96b0ab1b69ec674c22415af64ce356ea1
GOWTHAMAN7 <35653111+GOWTHAMAN7@users.noreply.github.com>
2018-01-25 16:45:10 +0530

    Create posorneg.c

---
+++ posorneg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number\n");
+scanf("%d",&n);
+if(n<=0)
+printf("it is positive \n");
+else
+printf("it is negative \n);
+getch;
+}
